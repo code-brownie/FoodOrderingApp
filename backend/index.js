b15e6558f2d6f761d46c486b604fc459def7f5ae
Aman <jhonman02@gmail.com>
2023-05-23 23:48:35 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const cors = require("cors");
-
-const app = express()
-const port = 5000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
 const mongoDB = require('./db');
+
 mongoDB();
-app.use(cors({credentials:true,origin:"https://foodapp12345.onrender.com"}))
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://foodapp12345.onrender.com);
-//   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
-//   next();
-// })
+
+const corsOptions = {
+  origin: 'https://foodapp-yp71.onrender.com',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api', require('./Routes/Createuser'));
 app.use('/api', require('./Routes/Displaydata'));
 app.use('/api', require('./Routes/OrdersData'));
+
 app.listen(port, () => {
-  console.log(`Food app listening on {process.env.REACT_APP_BASE_URL}:${port}`)
-})
+  console.log(`Food app listening on {process.env.REACT_APP_BASE_URL}:${port}`);
+});
